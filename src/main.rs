d4b481c013ffba5b24b265ebad3bd1f4d0983934
Cuong Nguyen <ancuongnguyen07@gmail.com>
2023-10-10 22:56:28 +0300

    [finalize app] integrate modules into executable command from cli

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,20 @@ mod commands;
 mod displayable_vec;
 mod png;
 
+use args::{App, Command};
+use clap::Parser;
+use commands::*;
+
 pub type Error = Box<dyn std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
 
 fn main() -> Result<()> {
-    todo!()
+    let cli = App::parse();
+
+    match cli.command {
+        Command::Decode(args) => decode(args),
+        Command::Encode(args) => encode(args),
+        Command::Remove(args) => remove(args),
+        Command::Print(args) => print_chunks(args),
+    }
 }
